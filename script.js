d3f4d9b9330b1f638a1cdc56ba41d4873f653c88
raditya1ty <alifiansyahaji1106@gmail.com>
2025-01-26 22:36:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const content = document.getElementById("content");
+    content.classList.add("visible");
+});
+
+
+
+import React from 'react'
+
+const script = () => {
+  return (
+    <div>
+      <p> halo teman teman</p>
+    </div>
+  )
+}
+
+export default script
